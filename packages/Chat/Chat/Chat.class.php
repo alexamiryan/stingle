f947ba8b56484ac4b0ee842a2cbd73d9b2cfd4c5
Alex Amiryan <alex@amiryan.org>
2010-10-12 17:23:35 +0500

    New chat and some enhancements in core

--- packages/Chat/Chat/Chat.class.php
+++ packages/Chat/Chat/Chat.class.php
@@ -1,252 +1,138 @@
 <?php
-
-class Chat extends DbAccessor{
-
-	const LIVE_MESSAGES = 1;
-	const ARCHIVE_MESSAGES = 2;
-	const ALL_MESSAGES = 4;
-
-	const TBL_MAIN = "wcs_messages";
-	const TBL_ARCHIVE = "wcs_archive";
-	const TBL_ONLINE = "chat_tran";
+class Chat extends Filterable
+{
+	const TBL_CHAT = 'chat';
 	
-	private $cache_count;
-	private $messages_count;
-
-	/**
-	 * Class constructor
-	 *
-	 * @return Chat instance
-	 * @version 1.0
-	 */
+	const STATUS_READ_UNREAD = 0;
+	const STATUS_READ_READ = 1;
+	
+	const FILTER_ID_FIELD = "id";
+	const FILTER_SENDER_USER_ID_FIELD = "sender_user_id";
+	const FILTER_RECEIVER_USER_ID_FIELD = "receiver_user_id";
+	const FILTER_DATETIME_FIELD = "datetime";
+	const FILTER_MESSAGE_FIELD = "message";
+	const FILTER_READ_FIELD = "read";
+	
 	public function __construct(){
 		parent::__construct();
 	}
-
-	public static function setProperties(Config $config){
-		$this->cache_count = $config->cache_count;
-		$this->messages_count = $config->messages_count;
-	}
-
-	private function archiveCache(){
-		$this->query->exec("LOCK TABLES `".self::TBL_MAIN."` WRITE,
-										`".self::TBL_ARCHIVE."` WRITE");
-		$this->query->exec("SELECT COUNT(`id`) AS msg_count FROM `" . self::TBL_MAIN . "`");
-		$msg_count = $this->query->fetchField("msg_count");
-		if($msg_count >= $this->cache_count + $this->messages_count){
-			$this->query->exec("SELECT UNIX_TIMESTAMP(`insert_date`) AS `udate` FROM `" . $self::TBL_MAIN. "` ORDER BY `insert_date` ASC LIMIT " . $this->cache_count . ", 1");
-			if(($date = $this->query->fetchField("udate"))){
-				if($this->query->exec("INSERT INTO `" . self::TBL_ARCHIVE . "` SELECT `transaction_id`,`sender`,`message`,`insert_date` FROM `" . self::TBL_MAIN . "` WHERE UNIX_TIMESTAMP(`insert_date`)< '$date'")){
-					$this->query->exec("DELETE from `" . self::TBL_MAIN . "` WHERE UNIX_TIMESTAMP(`insert_date`) < '$date'");
-					$this->query->exec("UNLOCK TABLES");
-					return true;
-				}
-				else{
-					$this->query->exec("UNLOCK TABLES");
-					return false;
-				}
-			}
-		}
-		$this->query->exec("UNLOCK TABLES");
-	}
-
-	private function getTableQuery($value){
-		switch($value){
-			case self::ARCHIVE_MESSAGES :
-				return "SELECT * FROM `" . self::TBL_ARCHIVE . "`";
-			case self::ALL_MESSAGES :
-				return "SELECT `transaction_id`,`sender`,`message`,`insert_date` FROM `" . self::TBL_MAIN . "` UNION SELECT `transaction_id`,`sender`,`message`,`insert_date` FROM `" . self::TBL_ARCHIVE . "`";
-			case self::LIVE_MESSAGES :
-			default :
-				return "SELECT * FROM `" . self::TBL_MAIN . "`";
-		}
-	}
-
-	/**
-	 * Enter description here...
-	 *
-	 * @param array
-	 * @return array
-	 */
-
-	private function getWhereClause($parameters){
-		$ret_array = array();
-		$param_count = count($parameters);
-		switch($param_count){
-			case 1 :
-				if(is_int($parameters[0])){
-					$ret_array["offset"] = $parameters[0];
-				}
-				else{
-					$date_from = strtotime($parameters[0]);
-					$ret_array["sql_query"] = "UNIX_TIMESTAMP(`insert_date`)>=" . $date_from;
-				}
-				break;
-			case 2 :
-				if(is_int($parameters[0])){
-					$ret_array["offset"] = $parameters[0];
-					$ret_array["length"] = $parameters[1];
-				}
-				else{
-					$date_from = $parameters[0];
-					if(is_int($parameters[1])){
-						$ret_array["offset"] = $parameters[1];
-						$ret_array["sql_query"] = "UNIX_TIMESTAMP(`insert_date`)>=" . $date_from;
-					}
-					else{
-						$date_to = $parameters[1];
-						$ret_array["sql_query"] = "UNIX_TIMESTAMP(`insert_date`)>=" . $date_from . " and  UNIX_TIMESTAMP(`insert_date`)<" . $date_to;
-					}
-				}
-				break;
-			case 3 :
-				if(is_int($parameters[1])){
-					$ret_array["offset"] = $parameters[1];
-					$ret_array["length"] = $parameters[2];
-				}
-				else{
-					$date_from = $parameters[0];
-					$date_to = $parameters[1];
-					$ret_array["offset"] = $parameters[2];
-					$ret_array["sql_query"] = "UNIX_TIMESTAMP(`insert_date`)>=" . $date_from . " and  UNIX_TIMESTAMP(`insert_date`)<" . $date_to;
-				}
-				break;
-			case 4 :
-				$date_from = $parameters[0];
-				$date_to = $parameters[1];
-				$ret_array["offset"] = $parameters[2];
-				$ret_array["length"] = $parameters[3];
-				$ret_array["sql_query"] = "UNIX_TIMESTAMP(`insert_date`)>=" . $date_from . " and  UNIX_TIMESTAMP(`insert_date`)<" . $date_to;
-			default :
-				break;
+	
+	protected function getFilterableFieldAlias($field){
+		switch($field){
+			case self::FILTER_ID_FIELD :
+			case self::FILTER_SENDER_USER_ID_FIELD :
+			case self::FILTER_RECEIVER_USER_ID_FIELD :
+			case self::FILTER_DATETIME_FIELD :
+			case self::FILTER_MESSAGE_FIELD :
+			case self::FILTER_READ_FIELD :
+				return "chat";
 		}
 
-		return $ret_array;
+		throw new RuntimeException("Specified field does not exist or not filterable");
 	}
-
-	public function send($transaction_id, $sender, $message){
-		$this->archiveCache();
-		$this->query->exec("INSERT INTO `" . self::TBL_MAIN . "` (`transaction_id`, `sender`, `message`, `insert_date`) VALUES ('$transaction_id', '$sender', '$message', now())");
-		if($this->query->affected() == 1){
-			return $this->query->getLastInsertId();
+	
+	public function isUnreadMessages($receiverUserId){
+		if(empty($receiverUserId) or !is_numeric($receiverUserId)){
+			throw new InvalidArgumentException("Invalid receiverUserId specified!");
+		}
+		
+		$unreadsCount = $this->query->exec("	SELECT COUNT(*) as `cnt` 
+												FROM `".self::TBL_CHAT."` 
+												WHERE `receiver_user_id`='$receiverUserId' and `read`=0")->fetchField('cnt');
+		if($unreadsCount > 0){
+			return true;
 		}
 		return false;
 	}
-
-	/**
-	 * Enter description here...
-	 *
-	 * @param messages, transaction_id, username
-	 * @param messages, transaction_id, username, offset
-	 * @param messages, transaction_id, username, offset, length
-	 * @param messages, transaction_id, username, from_date
-	 * @param messages, transaction_id, username, from_date, to_date
-	 * @param messages, transaction_id, username, from_date, offset
-	 * @param messages, transaction_id, username, from_date, offset, length
-	 * @param messages, transaction_id, username, from_date, to_date, offset
-	 * @param messages, transaction_id, username, from_date, to_date, offset, length
-	 * @var messages = { Chat::LIVE_MESSAGES | Chat::ARCHIVED_MESSAGES | Chat::ALL_MESSAGES }
-	 * @return array
-	 */
-
-	public function getMessages(){
-		$arguments = func_get_args();
-		$args_count = func_num_args();
-
-		if(($sql_query = $this->getTableQuery(array_shift($arguments)))){
-			if($args_count == 3){
-				$sql_query = $sql_query . " WHERE `transaction_id`='" . array_shift($arguments) . "' AND sender<>'" . array_shift($arguments) . "' AND `read`=0 ORDER BY `id` ASC";
-				$this->query->exec($sql_query);
-				if($this->query->countRecords()){
-					return $this->query->fetchRecords();
-				}
-			}
-			elseif($args_count > 3){
-				$sql_query = $sql_query . " WHERE `transaction_id`='" . array_shift($arguments) . "' AND sender<>'" . array_shift($arguments) . "' ORDER BY `id` ASC";
-
-				$where_array = $this->getWhereClause($arguments);
-				if(isset($where_array["sql_query"])){
-					$sql_query .= " AND " . $where_array["sql_query"];
-				}
-				$this->query->exec($sql_query);
-				if($this->query->countRecords()){
-					if(isset($where_array["offset"]) and isset($where_array["length"])){
-						return $this->query->fetchRecords($where_array["offset"], $where_array["length"]);
-					}
-					elseif(isset($where_array["offset"])){
-						return $this->query->fetchRecords($where_array["offset"]);
-					}
-					else{
-						return $this->query->fetchRecords();
-					}
-				}
-			}
+	
+	public function getUnreadMessagesInfo($receiverUserId){
+		if(empty($receiverUserId) or !is_numeric($receiverUserId)){
+			throw new InvalidArgumentException("Invalid receiverUserId specified!");
 		}
-		return array();
+		
+		$unreadsInfo = $this->query->exec("	SELECT `sender_user_id`, COUNT(`sender_user_id`) as `unread_count` 
+												FROM `".self::TBL_CHAT."` 
+												WHERE 	`receiver_user_id`='$receiverUserId' and 
+														`read`=0
+												GROUP BY `sender_user_id`")->fetchRecords();
+		
+		return $unreadsInfo;
 	}
-
-	public function makeRead($id_array){
-		if(count($id_array)){
-			foreach($id_array as $id){
-				$this->query->exec("UPDATE `".self::TBL_MAIN."` SET `read`=1 WHERE `id` = '$id'");
-			}
+	
+	public function getUnreadMessages($receiverUserId, $senderUserId){
+		if(empty($receiverUserId) or !is_numeric($receiverUserId)){
+			throw new InvalidArgumentException("Invalid receiverUserId specified!");
 		}
+		if(empty($senderUserId) or !is_numeric($senderUserId)){
+			throw new InvalidArgumentException("Invalid senderUserId specified!");
+		}
+		
+		$unreadMessages = $this->query->exec("	SELECT *, UNIX_TIMESTAMP(`datetime`) as `timestamp` 
+												FROM `".self::TBL_CHAT."` 
+												WHERE 	`receiver_user_id`='$receiverUserId' and 
+														`sender_user_id`='$senderUserId' and
+														`read`=0");
+		$unreadMessages = array();
+		while (($messageRow = $this->query->fetchRecord()) != false){
+			$message = new ChatMessage();
+			$message->id = $messageRow['id'];
+			$message->senderUserId = $messageRow['sender_user_id'];
+			$message->receiverUserId = $messageRow['receiver_user_id'];
+			$message->datetime = $messageRow['datetime'];
+			$message->timestamp = $messageRow['timestamp'];
+			$message->message = $messageRow['message'];
+			$message->read = $messageRow['read'];
+			
+			array_push($unreadMessages, $message);
+		}
+		return $unreadMessages;
 	}
-
-	/**
-	 * Enter description here...
-	 *
-	 * @param integer $transaction_id
-	 * @param string $field
-	 * @return unknown
-	 */
-	public function updateOnlineList($transaction_id, $field){
-		$now = time();
-		$this->query->exec("UPDATE `" . self::TBL_ONLINE . "` SET `$field`='$now' WHERE `id`='$transaction_id'");
-		return $now;
+	
+	public function setAsRead($receiverUserId, $senderUserId, $lastReceivedMessageId){
+		if(empty($receiverUserId) or !is_numeric($receiverUserId)){
+			throw new InvalidArgumentException("Invalid receiverUserId specified!");
+		}
+		if(empty($senderUserId) or !is_numeric($senderUserId)){
+			throw new InvalidArgumentException("Invalid senderUserId specified!");
+		}
+		if(empty($lastReceivedMessageId) or !is_numeric($lastReceivedMessageId)){
+			throw new InvalidArgumentException("Invalid lastReceivedMessageId specified!");
+		}
+		
+		$this->query->exec("	UPDATE `".self::TBL_CHAT."`
+								SET `read` = 1 
+								WHERE 	`receiver_user_id`='$receiverUserId' and 
+										`sender_user_id`='$senderUserId' and
+										`id` <= '$lastReceivedMessageId'");
 	}
-
-	function getTransaction($transaction_id){
-		$this->query->exec("SELECT * FROM `" . self::TBL_ONLINE . "` WHERE `id`='$transaction_id'");
-		if(($tran = $this->query->fetchRecord())){
-			return $tran;
+	
+	public function sendMessage(ChatMessage $chatMessage){
+		if(empty($chatMessage->senderUserId) or !is_numeric($chatMessage->senderUserId)){
+			throw new InvalidArgumentException("Invalid senderUserId specified!");
 		}
-		else{
-			return false;
+		if(empty($chatMessage->receiverUserId) or !is_numeric($chatMessage->receiverUserId)){
+			throw new InvalidArgumentException("Invalid receiverUserId specified!");
 		}
+		
+		$this->query->exec("	INSERT INTO `".self::TBL_CHAT."`
+										(`sender_user_id`, `receiver_user_id`, `message`) 
+								VALUES	('{$chatMessage->senderUserId}', '{$chatMessage->receiverUserId}', '{$chatMessage->message}')");
 	}
-
-	public function getLastPing($transaction_id, $field){
-		$this->query->exec("SELECT `$field` FROM `" . self::TBL_ONLINE . "` WHERE `id`='$transaction_id'");
-		if($this->query->countRecords() != 0){
-			return $this->query->fetchField($field);
-		}
-		else{
-			return false;
-		}
-	}
-
-	public function deleteFromOnlineList($transaction_id){
-		if($this->query->exec("DELETE FROM `" . self::TBL_ONLINE . "` WHERE `id`='$transaction_id'")){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-
-	public function deleteArchive(){
-		$arguments = func_get_args();
-
-		$sql_query = "DELETE FROM `" . self::TBL_ARCHIVE . "`";
-		$where_array = $this->getWhereClause($arguments);
-		if(isset($where_array["sql_query"])){
-			$sql_query .= " WHERE " . $where_array["sql_query"];
-		}
-
-		$this->query->exec($sql_query);
-		return $this->query->affected();
+	
+	public function getChatMessage($chatMessageId){
+		$messageRow = $this->query->exec("	SELECT *, UNIX_TIMESTAMP(`datetime`) as `timestamp` 
+												FROM `".self::TBL_CHAT."` 
+												WHERE 	`id`='$chatMessageId'");
+		$chatMessageObj = new ChatMessage();
+		$chatMessageObj->id = $messageRow['id'];
+		$chatMessageObj->senderUserId = $messageRow['sender_user_id'];
+		$chatMessageObj->receiverUserId = $messageRow['receiver_user_id'];
+		$chatMessageObj->datetime = $messageRow['datetime'];
+		$chatMessageObj->timestamp = $messageRow['timestamp'];
+		$chatMessageObj->message = $messageRow['message'];
+		$chatMessageObj->read = $messageRow['read'];
+		
+		return $chatMessageObj;
 	}
 }
-
 ?>

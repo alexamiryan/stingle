df045ffdfe9a89c5133f6026314f116aaa25ef2d
Lev Ananikyan <lev.ananikyan@gmail.com>
2010-10-15 19:08:38 +0500

    Improved JSON class

--- packages/JSON/JSON/JSON.class.php
+++ packages/JSON/JSON/JSON.class.php
@@ -5,7 +5,7 @@ class JSON
 	 * Make Json output and disable Smarty output
 	 * @param array $array
 	 */
-	public static function jsonOutput(array $array){
+	public static function jsonOutput($array){
 		$smartyConfig = ConfigManager::getConfig("Smarty");
 		
 		Reg::get($smartyConfig->Objects->Smarty)->disableOutput();
@@ -16,7 +16,7 @@ class JSON
 		echo self::jsonEncode($array);
 	}
 	
-	public static function jsonEncode(array $array){
+	public static function jsonEncode($array){
 		return json_encode($array);
 	}
 	

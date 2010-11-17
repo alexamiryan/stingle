429ff775ea32c4d8089ef456b7e3dad8ec24082b
Alex Amiryan <alex@amiryan.org>
2010-11-17 17:33:27 +0400

    Fixed bug in GeoIP

--- packages/GeoIP/GeoIP/LoaderGeoIP.class.php
+++ packages/GeoIP/GeoIP/LoaderGeoIP.class.php
@@ -3,6 +3,7 @@ class LoaderGeoIP extends Loader{
 	
 	protected function includes(){
 		require_once ('GeoIP.class.php');
+		require_once ('GeoLocation.class.php');
 	}
 	
 	protected function customInitBeforeObjects(){

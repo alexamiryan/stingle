57ad0e1b9091a562af198e5124be0cace878dccb
Alex Amiryan <alex@amiryan.org>
2010-11-17 17:41:33 +0400

    More enhancements in GeoIP plugin

--- packages/GeoIP/GeoIP/GeoIP.class.php
+++ packages/GeoIP/GeoIP/GeoIP.class.php
@@ -35,17 +35,8 @@ class GeoIP extends DbAccessor
 	 * Get country code by IP
 	 */
 	public function getCountryCode($ip = null){
-		if($ip === null){
-			$ip = $_SERVER['REMOTE_ADDR'];
-		}
-
-		$this->query->exec("SELECT `country` FROM ".Tbl::get('TBL_BLOCKS')."
-							LEFT JOIN ".Tbl::get('TBL_LOCATIONS')." USING (`locId`)
-							WHERE index_geo = INET_ATON('".$ip."')-(INET_ATON('".$ip."')%65536) AND INET_ATON('".$ip."') BETWEEN `startIpNum` AND `endIpNum`");
-		if($this->query->countRecords()){
-			return $this->query->fetchField('country');
-		}		
-		return null;
+		$location = static::getLocation($ip);
+		return $location->country;
 	}
 }
 ?>

65c70f76f87f33748a2cb685f00689c55746bf81
Lev Ananikyan <lev.ananikyan@gmail.com>
2010-11-19 12:13:10 +0400

    Chat begin

--- packages/Chat/Chat/DefaultConfig.inc.php
+++ packages/Chat/Chat/DefaultConfig.inc.php
@@ -1,9 +1,13 @@
 <?
+/**
+ * ManagerAlgorithm = [byLastId, byMakeRead]
+ */
 $defaultConfig = array(	
 						'cache_count' => 150,
 						'messages_count' => 150,
 						'connect_timeout' => 30,
 						'reconnect_timeout' => 100,
-						'Objects' => array(	'Chat' => 'chat' )
+						'Objects' => array(	'ChatManager' => 'chatMgr' ),
+						'ManagerAlgorithm' => 'byLastId'
 					);
 ?>

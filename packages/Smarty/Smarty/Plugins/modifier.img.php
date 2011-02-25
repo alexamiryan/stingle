a242d9fdfa4451ed33dc6182c9552e35225ff416
Vahan Mnatsakanyan <vahan@mnaco.co.uk>
2011-02-25 16:03:44 +0400

    Fixed bug in img smarty modifier

--- packages/Smarty/Smarty/Plugins/modifier.img.php
+++ packages/Smarty/Smarty/Plugins/modifier.img.php
@@ -5,6 +5,8 @@
  */
 
 function smarty_modifier_img($filename){
-	return Reg::get(ConfigManager::getConfig('Smarty', 'Smarty')->Objects->Smarty)->getCurrentTemplatePath() .'img/'. $filename;
+	/* @var $smarty SamrtyWrapper */
+	$smarty = Reg::get(ConfigManager::getConfig('Smarty', 'Smarty')->Objects->Smarty);
+	return $smarty->template_dir . $smarty->getCurrentTemplatePath() .'img/'. $filename;
 }
 ?>

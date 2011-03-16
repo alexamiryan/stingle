e058fe7189605b3fe627ac027c96c09588dd52e0
Lev Ananikyan <lev.ananikyan@gmail.com>
2011-03-16 16:15:03 +0400

    Add hooks before and after Controller

--- index.php
+++ index.php
@@ -4,7 +4,11 @@ require_once (SITE_CONFIGS_PATH . "config.inc.php");
 
 require_once (STINGLE_PATH . "init/init.php");
 
+HookManager::callHook("BeforeController");
+
 HookManager::callHook("Controller");
+
+HookManager::callHook("AfterController");
 
 HookManager::callHook("InitEnd");
 ?>

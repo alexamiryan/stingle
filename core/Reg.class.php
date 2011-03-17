f0ae95e7dd5201ed94dd6c0944b2e087d1ace488
Alex Amiryan <alex@amiryan.org>
2011-03-17 14:55:05 +0400

    Added Smarty class to Reg::get suggestions

--- core/Reg.class.php
+++ core/Reg.class.php
@@ -20,7 +20,7 @@ class Reg
 	 * 
 	 * @param string $key
 	 * @param bool $throwException
-	 * @return PackageManager|MysqlQuery|UserManagement|LanguageManager|Nav
+	 * @return PackageManager|MysqlQuery|UserManagement|LanguageManager|Nav|SmartyWrapper
 	 */
 	public static function get($key){
 		if(!static::isRegistered($key)){

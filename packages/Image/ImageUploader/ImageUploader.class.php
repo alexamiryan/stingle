25b1cd325c38bd75a68e6a8e9eba7204e2dbf7b2
Alex Amiryan <alex@amiryan.org>
2011-04-15 17:09:54 +0500

    Improved ImageUploader plugin's static method upload.

--- packages/Image/ImageUploader/ImageUploader.class.php
+++ packages/Image/ImageUploader/ImageUploader.class.php
@@ -7,11 +7,9 @@ class ImageUploader
 	
 	const EXCEPTION_IMAGE_IS_SMALL = 1;
 	
-	public static function upload($file, $fileName = null, $uploadDir = null){
-		$imageUploaderConfig = ConfigManager::getConfig("Image", "ImageUploader")->AuxConfig;
-		if($uploadDir === null and isset($imageUploaderConfig->uploadDir)){
-			$uploadDir = $imageUploaderConfig->uploadDir;
-		}
+	public static function upload($file, $fileName = null, Config $imageUploaderConfig = null){
+		$imageUploaderConfig = ConfigManager::mergeConfigs($imageUploaderConfig, ConfigManager::getConfig("Image", "ImageUploader")->AuxConfig);
+		$uploadDir = $imageUploaderConfig->uploadDir;
 		
 		if(empty($uploadDir)){
 			throw new RuntimeException("Unable to get any appropriate uploadDir!");

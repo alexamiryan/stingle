72e571345dac51fcd5be2b71cd6fa0d82ba96a1e
Lev Ananikyan <lev.ananikyan@gmail.com>
2011-04-29 18:21:46 +0500

    Disabled photoCache plugin to create global object.

--- packages/Image/PhotoCache/LoaderPhotoCache.class.php
+++ packages/Image/PhotoCache/LoaderPhotoCache.class.php
@@ -3,9 +3,5 @@ class LoaderPhotoCache extends Loader{
 	protected function includes(){
 		require_once ('PhotoCache.class.php');
 	}
-	
-	protected function loadPhotoCache(){
-		Reg::register($this->config->Objects->PhotoCache, new PhotoCache($this->config->AuxConfig));
-	}
 }
 ?>

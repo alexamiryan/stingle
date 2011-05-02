2765cbf2a3ea5794f8fc7dc805e1c21a3fbd38c6
Lev Ananikyan <lev.ananikyan@gmail.com>
2011-05-02 14:03:01 +0500

    photo cache enhancement

--- packages/Image/PhotoCache/SmartyPlugins/modifier.imgGen.php
+++ packages/Image/PhotoCache/SmartyPlugins/modifier.imgGen.php
@@ -7,11 +7,13 @@
  * @param string $sizeName
  * @return string
  */
-function smarty_modifier_imgGen(Photo $photo, $sizeName){
+function smarty_modifier_imgGeno(Photo $photo, $sizeName){
+	
 	if(empty($sizeName)){
 		return;
 	}
-	
+	exit;
+
 	$resultingFilePath = Reg::get('photoCache')->generateImageCache($sizeName, $photo);
 
 	return SITE_PATH . $resultingFilePath;

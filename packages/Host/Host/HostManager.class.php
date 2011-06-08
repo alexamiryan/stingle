f90a3503d4e1f9a156008b393f691766934915d2
Alex Amiryan <alex@amiryan.org>
2011-06-08 18:34:54 +0500

    Fixed notice in HostManager when calling from CGI

--- packages/Host/Host/HostManager.class.php
+++ packages/Host/Host/HostManager.class.php
@@ -69,7 +69,7 @@ class HostManager{
 		}
 		
 		$page_url = "";
-		if($_SERVER["SERVER_PORT"] != "80"){
+		if(isset($_SERVER["SERVER_PORT"]) and $_SERVER["SERVER_PORT"] != "80"){
 			$page_url .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
 		}
 		else{

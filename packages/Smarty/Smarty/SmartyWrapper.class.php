6339c51bdebb62c01a4daba53f4e69cd4932d987
Alex Amiryan <alex@amiryan.org>
2011-10-19 16:06:56 +0500

    Added getTemplate method to SmartyWrapper

--- packages/Smarty/Smarty/SmartyWrapper.class.php
+++ packages/Smarty/Smarty/SmartyWrapper.class.php
@@ -223,6 +223,13 @@ class SmartyWrapper extends Smarty {
 		array_push($this->plugins_dir, $pluginDir);
 	}
 	
+	/**
+	 * Set template
+	 * 
+	 * @param string $template
+	 * @throws InvalidArgumentException
+	 * @throws RuntimeException
+	 */
 	public function setTemplate($template){
 		if(!is_dir($this->template_dir."templates/".$template)){
 			throw new InvalidArgumentException("Specified templates directory (".$this->template_dir."templates/".$template.") doesn't exist");
@@ -234,6 +241,14 @@ class SmartyWrapper extends Smarty {
 		
 		$this->templateExtends = $this->templatesConfig->templates->$template;
 		$this->template = $template;
+	}
+	
+	/**
+	 * Get current template name
+	 * @return string
+	 */
+	public function getTemplate(){
+		return $this->template;
 	}
 	
 	/**

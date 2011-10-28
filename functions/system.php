acb697b9c0e62ec1b115a64ebaba235a82e69886
Alex Amiryan <alex@amiryan.org>
2011-10-28 13:32:35 +0500

    Removed bad code from default exception handler

--- functions/system.php
+++ functions/system.php
@@ -1,22 +1,12 @@
 <?
 function default_exception_handler(Exception $e){
-	global $site_name;
-	
 	HookManager::callHook('NoDebugExceptionHandler', array('e' => $e));
 	
-	$config = ConfigManager::getGlobalConfig();
 	if(Debug::getMode()){
 		echo format_exception($e, true);
 	}
 	else{
 		HookManager::callHook('ExceptionHandler', array('e' => $e));
-	}
-	if(Reg::get('packageMgr')->isPluginLoaded("Db","Db") and function_exists("write_log")){
-		@write_log("Exception", format_exception($e));
-	}
-	
-	if($config->Debug->send_email_on_exception and function_exists("send_mail")){
-		@send_mail($config->site->developer_mail, "Exception on $site_name", format_exception($e, true));
 	}
 	exit;
 }

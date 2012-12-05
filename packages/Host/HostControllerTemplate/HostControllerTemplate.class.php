761ac97c0ad1895eaf3ae3f8e9f965639503c255
Alex Amiryan <alex@amiryan.org>
2012-12-05 18:32:25 +0400

    Added method to change host's template and controller

--- packages/Host/HostControllerTemplate/HostControllerTemplate.class.php
+++ packages/Host/HostControllerTemplate/HostControllerTemplate.class.php
@@ -13,5 +13,29 @@ class HostControllerTemplate extends DbAccessor{
 		
 		return $sql->fetchRecord();
 	}
+	
+	public static function setControllerTemplateByHost(Host $host, $controller, $template){
+		$sql = MySqlDbManager::getQueryObject();
+		$qb = new QueryBuilder();
+		
+		if(!empty($controller) or !empty($template)){
+			$qb->insert(Tbl::get('TBL_HOST_CONTROLLER_TEMPLATE'))
+				->values(array(
+					'host_id' => $host->id,
+					'controller' => $controller,
+					'template' => $template
+			))
+			->onDuplicateKeyUpdate()
+			->set(new Field('controller'), $controller)
+			->set(new Field('template'), $template);
+		}
+		else{
+			$qb->delete(Tbl::get('TBL_HOST_CONTROLLER_TEMPLATE'))
+				->where($qb->expr()->equal(new Field('host_id'), $host->id));
+		}
+		$sql->exec($qb->getSQL());
+	
+		return $sql->affected();
+	}
 }
 ?>

0f08df07af322e7946f611a60cf9954882185339
Alex Amiryan <alex@amiryan.org>
2012-12-13 18:14:13 +0400

    Updated CometEvents dependencies

--- packages/Comet/CometEvents/DependencyCometEvents.class.php
+++ packages/Comet/CometEvents/DependencyCometEvents.class.php
@@ -4,6 +4,7 @@ class DependencyCometEvents extends Dependency
 	public function __construct(){
 		$this->addPlugin("Db", "Db");
 		$this->addPlugin("Db", "QueryBuilder");
+		$this->addPlugin("Users");
 	}
 }
 ?>

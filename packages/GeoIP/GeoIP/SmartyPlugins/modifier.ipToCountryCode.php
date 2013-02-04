f5bdd0ff87b50947f2d2411a07a413027691f92a
Lev Ananikyan <lev.ananikyan@gmail.com>
2013-02-04 12:42:58 +0400

    ipToCountryCode modifier added

---
+++ packages/GeoIP/GeoIP/SmartyPlugins/modifier.ipToCountryCode.php
@@ -0,0 +1,11 @@
+<?
+/**
+ * Get Country code from IP
+ *
+ * @param string $ip
+ * @return string
+ */
+function smarty_modifier_ipToCountryCode($ip){
+	return Reg::get('geoIp')->getCountryCode($ip, -1);
+}
+?>

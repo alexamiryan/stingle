97c6e24889dc0b8a071a36366c6ad75af8d54cfe
Alex Amiryan <alex@amiryan.org>
2013-04-17 18:05:47 +0400

    Reverted smart img modifier

--- packages/Output/Smarty/Plugins/modifier.img.php
+++ packages/Output/Smarty/Plugins/modifier.img.php
@@ -7,14 +7,5 @@
 function smarty_modifier_img($filename){
 	/* @var $smarty SamrtyWrapper */
 	$smarty = Reg::get(ConfigManager::getConfig("Output", "Smarty")->Objects->Smarty);
-    try{
-	    return SITE_PATH . $smarty->findFilePath('img/'. $filename);
-    }
-    catch(TemplateFileNotFoundException $e){
-        $config = ConfigManager::getGlobalConfig();
-        if($config->Debug->send_email_on_exception and Reg::isRegistered('mail')){
-            Reg::get('mail')->developer("Exception", "Can not find image ". $filename);
-        }
-        return SITE_PATH . $smarty->findFilePath('img/errors/no_photo.jpg');
-    }
+	return SITE_PATH . $smarty->findFilePath('img/'. $filename);
 }

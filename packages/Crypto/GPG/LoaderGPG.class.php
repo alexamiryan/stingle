09b6668d6188aee70dd9856708fbe3896f6d3054
Alex Amiryan <alex@amiryan.org>
2013-11-29 14:01:01 +0400

    Removed unnecessary include in GPG plugin

--- packages/Crypto/GPG/LoaderGPG.class.php
+++ packages/Crypto/GPG/LoaderGPG.class.php
@@ -3,7 +3,7 @@ class LoaderGPG extends Loader{
 	
 	protected function includes(){
 		// If you have PECL GnuPG package installed you don't need this include
-		stingleInclude ('Crypt/GPG.php');
+		//stingleInclude ('Crypt/GPG.php');
 		stingleInclude ('Managers/GPG.class.php');
 	}
 }

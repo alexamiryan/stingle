b20eabca7a8fbaff63dc3ee37e100c8d88e6189f
Alex Amiryan <alex@amiryan.org>
2016-06-14 23:20:42 +0400

    Fixed auto table import and added SQL files for new Profile plugin

---
+++ packages/Profile/Profile/SQL/profile_save.sql
@@ -0,0 +1,51 @@
+-- MySQL dump 10.13  Distrib 5.6.29, for Linux (x86_64)
+--
+-- Host: localhost    Database: edesirs
+-- ------------------------------------------------------
+-- Server version	5.6.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `profile_save`
+--
+
+DROP TABLE IF EXISTS `profile_save`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profile_save` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `key_id` int(10) unsigned NOT NULL,
+  `value_id` int(10) unsigned DEFAULT NULL,
+  `value_cust` text,
+  PRIMARY KEY (`id`),
+  KEY `key_id` (`key_id`),
+  KEY `value_id` (`value_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `profile_save_ibfk_1` FOREIGN KEY (`key_id`) REFERENCES `profile_keys` (`id`),
+  CONSTRAINT `profile_save_ibfk_2` FOREIGN KEY (`value_id`) REFERENCES `profile_values` (`id`),
+  CONSTRAINT `profile_save_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `wum_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-06-14 20:53:04
+

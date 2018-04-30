fa008e63f5ea89d329f833dd4828c30e29699ebb
Alex Amiryan <alex@amiryan.org>
2018-04-30 18:04:27 +0400

    Mail plugin SMTP update

--- packages/Mail/Mail/DefaultConfig.inc.php
+++ packages/Mail/Mail/DefaultConfig.inc.php
@@ -1,22 +1,36 @@
 <?php
-$defaultConfig = array(	
-	'AuxConfig' => array( 
+
+$defaultConfig = array(
+	'AuxConfig' => array(
 		'mailParams' => array(
-		    'fromMail' => 'no-reply@example.com',
-		    'fromName' => 'My Website',
-		    'replyToMail' => 'contact@example.com',
-		    'replyToName' => 'My Website Support',
+			'fromMail' => 'no-reply@example.com',
+			'fromName' => 'My Website',
+			'replyToMail' => 'contact@example.com',
+			'replyToName' => 'My Website Support'
 		),
-		'DKIM' =>array(
-		    'enabled' => false,
-		    'publicKey' => null,
-		    'privateKey' => null,
-		    'password' => null,
-		    'domain' => null,
-		    'selector' => 'stingle'
+		'DKIM' => array(
+			'enabled' => false,
+			'publicKey' => null,
+			'privateKey' => null,
+			'password' => null,
+			'domain' => null,
+			'selector' => 'stingle'
+		),
+		'SMTP' => array(
+			'enabled' => false,
+			'host' => null,
+			'port' => 25,
+			'secureMethod' => null,
+			'debug' => null,
+			'customOptions' => null,
+			'auth' => array(
+				'enabled' => false,
+				'username' => null,
+				'password' => null
+			)
 		)
 	),
 	'Objects' => array(
-	    'Mail' => 'mail'
+		'Mail' => 'mail'
 	)
 );
--- packages/Mail/Mail/Managers/MailSender.class.php
+++ packages/Mail/Mail/Managers/MailSender.class.php
@@ -25,13 +25,40 @@ class MailSender {
 	 * @access public
 	 * @return bool true if the mail was successfully accepted for delivery, false otherwise.
 	 */
-	public function send(Mail $mail) {
+	public function send(Mail $mail, Config $customConfig = null) {
 		if (empty($mail)) {
 			throw new MailException("Mail object is empty");
 		}
 
-		$phpMailer = new PHPMailer();
+		$config = clone($this->config);
+		if($customConfig){
+			$config = ConfigManager::mergeConfigs($customConfig, $config);
+		}
 		
+		$phpMailer = new PHPMailer();
+
+		if ($config->SMTP->enabled) {
+			$phpMailer->isSMTP();
+			$phpMailer->Host = $config->SMTP->host;
+			$phpMailer->Port = $config->SMTP->port;
+			
+			if($config->SMTP->debug){
+				$phpMailer->SMTPDebug = $config->SMTP->debug;
+			}
+			if ($config->SMTP->secureMethod) {
+				$phpMailer->SMTPSecure = $config->SMTP->secureMethod;
+			}
+			if ($config->SMTP->auth->enabled) {
+				$phpMailer->SMTPAuth = true;
+				$phpMailer->Username = $config->SMTP->auth->username;
+				$phpMailer->Password = $config->SMTP->auth->password;
+			}
+			
+			if($config->SMTP->customOptions){
+				$phpMailer->SMTPOptions = $config->SMTP->customOptions->toArray(true);
+			}
+		}
+
 		$phpMailer->setFrom($mail->from, $mail->fromName);
 
 		foreach ($mail->getToAddresses() as $address) {
@@ -41,7 +68,7 @@ class MailSender {
 		foreach ($mail->getReplyToAddresses() as $address) {
 			$phpMailer->addReplyTo($address['address'], $address['name']);
 		}
-		
+
 		foreach ($mail->getCustomHeaders() as $header) {
 			$phpMailer->addCustomHeader($header['name'], $header['value']);
 		}
@@ -49,24 +76,29 @@ class MailSender {
 		$phpMailer->Subject = $mail->subject;
 		$phpMailer->isHTML(true);
 		$phpMailer->CharSet = $mail->charSet;
-		
+
 		$phpMailer->Body = $mail->htmlBody;
 		if (!empty($mail->textBody)) {
 			$phpMailer->AltBody = $mail->textBody;
 		}
 
-		if ($this->config->DKIM->enabled) {
-			$phpMailer->DKIM_domain = $this->config->DKIM->domain;
-			$phpMailer->DKIM_private_string = $this->config->DKIM->privateKey;
-			$phpMailer->DKIM_selector = $this->config->DKIM->selector;
-			$phpMailer->DKIM_passphrase = $this->config->DKIM->password;
+		if ($config->DKIM->enabled) {
+			$phpMailer->DKIM_domain = $config->DKIM->domain;
+			$phpMailer->DKIM_private_string = $config->DKIM->privateKey;
+			$phpMailer->DKIM_selector = $config->DKIM->selector;
+			$phpMailer->DKIM_passphrase = $config->DKIM->password;
 			$phpMailer->DKIM_identity = $phpMailer->From;
 		}
 
 
-		if(!$phpMailer->send()){
-			throw new MailException("Error sending email: " . $phpMailer->ErrorInfo);
+		if (!$phpMailer->send()) {
+			$error = $phpMailer->ErrorInfo;
+			if ($config->SMTP->enabled && $config->SMTP->debug){
+				$error .= "\n\nDebug\n\n" . $phpMailer->Debugoutput;
+			}
+			throw new MailException("Error sending email: " . $error);
 		}
 		return true;
 	}
+
 }

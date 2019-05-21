51a40ac04b795b98d90acbda08bc302e0ba6c117
Alex Amiryan <alex@amiryan.org>
2019-05-21 16:22:12 +0400

    Quick fix for SNS bounces

--- packages/Mail/EmailStats/LoaderEmailStats.class.php
+++ packages/Mail/EmailStats/LoaderEmailStats.class.php
@@ -30,15 +30,25 @@ class LoaderEmailStats extends Loader{
 			return;
 		}
 		$stat = Reg::get('emailStats')->getEmailStatById($args['mailId']);
+		$msgHeaders = "";
+		$msgBody = "";
+		
+		if(!empty($args['msgHeaders'])){
+			$msgHeaders = $args['msgHeaders'];
+		}
+		if(!empty($args['msgBody'])){
+			$msgBody = $args['msgBody'];
+		}
+		
 		if($stat){
 			if($args['bounceType'] == MailSender::BOUNCE_TYPE_BLOCKED){
-				Reg::get('emailStats')->setEmailAsBouncedBlock($stat->emailId, $args['msgHeaders'], $args['msgBody']);
+				Reg::get('emailStats')->setEmailAsBouncedBlock($stat->emailId, $msgHeaders, $msgBody);
 			}
 			elseif($args['bounceType'] == MailSender::BOUNCE_TYPE_HARD){
-				Reg::get('emailStats')->setEmailAsBouncedHard($stat->emailId, $args['msgHeaders'], $args['msgBody']);
+				Reg::get('emailStats')->setEmailAsBouncedHard($stat->emailId, $msgHeaders, $msgBody);
 			}
 			else{
-				Reg::get('emailStats')->setEmailAsBouncedSoft($stat->emailId, $args['msgHeaders'], $args['msgBody']);
+				Reg::get('emailStats')->setEmailAsBouncedSoft($stat->emailId, $msgHeaders, $msgBody);
 			}
 		}
 	}

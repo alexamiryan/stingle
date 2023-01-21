cb743703840776ef2db91b05ace9b6c2750b0db8
Alex Amiryan <alex@amiryan.org>
2023-01-22 03:32:20 +0400

    Now you can define ADDONS_FOLDER_PATH as folder of collection of addons or ADDONS_PATH as collection of addons directly

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
+if(defined('ADDONS_FOLDER_PATH') && is_dir(ADDONS_FOLDER_PATH) && !defined('ADDONS_PATHS')) {
+    $addonsPath = ADDONS_FOLDER_PATH;
+    $folders = [];
+    if(substr($addonsPath, strlen($addonsPath) - 1) !== '/'){
+        $addonsPath .= '/';
+    }
+    $files = scandir($addonsPath);
+    foreach($files as $file) {
+        if(is_dir($addonsPath . $file) && $file != '.' && $file != '..') {
+            $folders[] = $addonsPath . $file;
+        }
+    }
+    define ("ADDONS_PATHS", $folders);
+}
+
 if(defined('DISABLE_APCU') || !extension_loaded('apcu') || !apcu_exists('globalConfig') ){
 	require_once (SITE_CONFIGS_PATH . "config.inc.php");
     
